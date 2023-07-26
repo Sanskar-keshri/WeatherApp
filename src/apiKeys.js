de02a9d44382af1351eb2add1b35b78548964749
Sanskar-Keshri <sanskarkeshri007@gmail.com>
2023-07-26 06:00:37 +0530

    Weather App ReactJS

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "8d9f91a4d5bc16ddd63451cd78559d5e",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
